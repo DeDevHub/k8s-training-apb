3043632275ccf8ac89045a7cf08eb8f3ff546dcb
Khatthaphone <khatthaphonez@gmail.com>
2020-12-13 11:47:27 +0700

    Initial commit

---
+++ week1/node-app/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  return res.send("<html><body>Sample made with ♥️</body></html>");
+});
+
+const port = process.env.port | 8080;
+
+app.listen(port, () => {
+  console.log("listening on port:", port);
+});
---
+++ week1/node-upload/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyPasrser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+app.get("/", (req, res) => {
+  return res.send("<html><body>Sample made with ♥️</body></html>");
+});
+
+app.use(bodyPasrser.urlencoded({ extended: true }));
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+  }),
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) return res.status(400).json({ message: "No file" });
+  return res.send(req.file);
+});
+
+const port = process.env.port | 8080;
+
+app.listen(port, () => {
+  console.log("listing on port:", port);
+});
