5a416f758c1dbedfba213470745cf4e87467ff10
Khamphai KHOUNNAVONGSA <pphai67@gmail.com>
2021-01-17 09:27:57 +0700

    Demo2 add profile

--- week4/demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ week4/demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -2,6 +2,7 @@ package com.example.demo2;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Demo2Application {
 
+    @Value("${app.active}")
+    private String profileActive;
+
+    @Value("${key.signature}")
+    private String keySignature;
+
     private static final Logger log = LoggerFactory.getLogger(Demo2Application.class.getName());
 
     public static void main(String[] args) {
@@ -20,7 +27,13 @@ public class Demo2Application {
 
     @GetMapping("/demo2")
     public ResponseEntity<String> hello(){
-        log.info("Hello demo service 2........");
-        return ResponseEntity.ok("Hello demo service 2........");
+        log.info("Hello demo service 2........ " + profileActive);
+        return ResponseEntity.ok("Hello demo service 2........ " + profileActive);
+    }
+
+    @GetMapping("/key")
+    public ResponseEntity<String> key(){
+        log.info("Hello demo service 2........ KEY = " + keySignature);
+        return ResponseEntity.ok("Hello demo service 2........ KEY = " + keySignature);
     }
 }
